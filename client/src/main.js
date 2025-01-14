bf2cbcd70e7882dd33ec05c4918ff4e101388948
Augment Chinedu <augmentchinedu@gmail.com>
2025-01-14 08:04:21 +0100

    Service Worker Added for PWA

--- client/src/main.js
+++ client/src/main.js
@@ -12,3 +12,10 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./service/index.js')
+    .then((reg) => console.log('Service Worker registered:', reg))
+    .catch((err) => console.error('Service Worker registration failed:', err))
+}
